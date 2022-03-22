a4e0eccf5756b76e0059a8d4d3349338fad527a2
kevincavi <kevincasanovasvila@gmail.com>
2022-03-22 21:26:36 +0100

    k

---
+++ Pagina web/inici.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+
+  <style media="screen">
+
+body{
+  background-color:white
+}
+
+#logofira{
+  display:block;
+  position:absolute;
+  width:320px;
+  height:320px;
+  background-color: transparent;
+  transform:translate(10%, -60%);
+  cursor:pointer;
+  transition: all 400ms ease;
+}
+
+#logolletres{
+  display:block;
+  position:absolute;
+  width:400px;
+  height:400px;
+  transform:translate(0%, -7%);
+  transition: all 400ms ease;
+}
+
+.logosvg{
+  width:300px;
+  height:300px;
+}
+
+.lletres{
+  width:350px;
+  height:350px;
+}
+
+#buttonClicker{
+  display:block;
+  position:absolute;
+  width:400px;
+  height:400px;
+  background-color:transparent;
+  transform:translate(-50%, -50%);
+  left:50vw;
+  top:50vh;
+  cursor:pointer;
+  transition: all 400ms ease;
+  border: none;
+  outline: hidden;
+}
+
+
+
+  </style>
+  <body>
+    <a href=""><button type="button" id="buttonClicker">
+    <div id="logolletres">
+      <img class="C:\xampp\htdocs\projects\sintesi\assets\logo-oritzontal.svg" />
+    </div>
+  </button>
+  </body>
+</html>
