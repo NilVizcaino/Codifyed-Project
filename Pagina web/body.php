0bc869b9ee86618a1472f0cc00de174b28bf1773
kevincavi <kevincasanovasvila@gmail.com>
2022-03-25 21:09:23 +0100

    a

--- Pagina web/body.php
+++ Pagina web/body.php
@@ -1,10 +1,46 @@
 <!DOCTYPE html>
-<html lang="ca" dir="ltr">
+<html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title></title>
+    <style>
+    div#wrapp{
+      display:grid;
+      position:relative;
+      width:70vw;
+      height:300px;
+      margin:-70px auto 200px auto;
+      grid-template-columns:1fr 1fr 1fr;
+      grid-column-gap:10px;
+    }
+
+    section{
+      display:flex;
+      position:relative;
+      justify-content:center;
+      align-items:center;
+    }
+
+    #productes{
+      width:450px;
+      height:450px;
+      cursor:pointer;
+      transition: all 300ms ease;
+    }
+
+    #productes:hover{
+      width:475px;
+      height:475px;
+    }
+
+    </style>
   </head>
   <body>
-  <h1>hola mon</h1>
+    <div id="wrapp">
+      <section>Box 1</section>
+      <section><img id="productes" src="productes.png"/>
+</section>
+      <section>Box 3</section>
+    </div>
   </body>
 </html>
