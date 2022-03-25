18446f2311ea06e135f08deb8583d730dc2259cc
kevincavi <kevincasanovasvila@gmail.com>
2022-03-25 17:51:24 +0100

    m

---
+++ Pagina web/footer.php
@@ -0,0 +1,15 @@
+<footer>
+  <div id="info">
+    <span>Carrer Muntaner, 507</span>
+    <span>BARCELONA</span>
+    <span>T: 93 850 60 64</span>
+  </div>
+  <div id="centre">
+    <span><i class="fa fa-facebook" onclick="window.open('https://www.facebook.com/lasallemanlleu.cat/', '_blank');"></i></span>
+    <span><i class="fa fa-instagram" onclick="window.open('https://www.instagram.com/lasallemanlleu/','_blank');"></i></span>
+    <span><i class="fa fa-twitter" onclick="window.open('https://twitter.com/lasallemanlleu','_blank');"></i></span>
+    <span><i class="fa fa-youtube" onclick="window.open('https://www.youtube.com/channel/UC_YT1VmIypAUGsx13Ed9pAg','_blank');"></i></span>
+  </div>
+</footer>
+</body>
+</html>
