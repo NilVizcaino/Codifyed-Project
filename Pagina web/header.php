70871f1a1e4fcac55e7bdaa057957f9146d80bc8
Nil <kickcreator@gmail.com>
2022-04-04 17:11:31 +0200

    adf

--- Pagina web/header.php
+++ Pagina web/header.php
@@ -4,17 +4,31 @@
     <meta charset="utf-8">
     <title></title>
     <link rel="stylesheet" type="text/css"  href="style.css" />
+    <script>
+      var Posició = 0;
+      let submenu = document.getElmentById("submenu");
+      function sumar(){
+        sumar = sumar + 1;
+
+        if(sumar == 1){
+        }
+
+        if(sumar == 2){
+          Posició == 0
+        }
+      }
+    </script>
   </head>
 <body>
   <a name="capçalera"></a>
   <header>
     <nav>
-      <button id="registrar-se"><a  href="registre.php">Registrar-se</a></button>
+      <button id="registrar-se"><a href="registre.php">Registrar-se</a></button>
       <button id="unoa"><a  href="login.php">Iniciar sessió</a></button>
-      <button id="menu">Menu</button>
-      <button id="close">Tencar</button>
+      <button id="menu" onclick="sumar();">Menu</button>
+      <button id="close" onclick="sumar();">Tencar</button>
     <div id="submenu">
-      <button id="idioma"><b>Inici</b></button>
+      <button id="idioma"><b><a href="registre.php">Inici</a></b></button>
       <button id="idioma"><b>Idioma</b></button>
       <button id="qui"><b>Qui Som?</b></button>
     </div>
