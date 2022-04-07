932c6e38f95e68ff2ae70e9d1a50924117a8a049
Nil <kickcreator@gmail.com>
2022-04-07 21:32:04 +0200

    trabajito

--- Pagina web/qui_som.php
+++ Pagina web/qui_som.php
@@ -4,7 +4,7 @@
 <div id=div_paragref_qui_som>
 <p>La nostra empresa es catalana, tenim botiga online i presencial situada al Carrer Muntaner, numero 507, Barcelona. La nostra empresa esta feta amb la intencio de ajudar i millorar la seguretat digital de tots els nostres clients.</p>
 <p>En cas de voler contectar amb nosaltres, podeu fer-ho a traves de les nostres xarxes socials, per Correu (CStaffSupport@service.com) o per Telefon (927 396 223).</p>
-
-<p>Nosaltres som codify-ed, la seguretat al teu costat.</p>
 </div>
+
+<div id="eslogan"><p>Nosaltres som codify-ed, la seguretat al teu costat.</p></div>
 <?php include("footer.php") ?>
