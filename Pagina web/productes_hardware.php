3ce55b67090884e791622023d40d9a3f71fd64c1
kevincavi <kevincasanovasvila@gmail.com>
2022-04-07 17:27:49 +0200

    d

--- Pagina web/productes_hardware.php
+++ Pagina web/productes_hardware.php
@@ -6,40 +6,40 @@
       position:relative;
       width:70vw;
       height:300px;
-      margin:-20px 110px 200px 125px;
-      grid-template-columns:1fr 1fr;
+      margin:-20px 110px 200px 175px;
+      grid-template-columns:1fr;
       grid-column-gap:25px;
     }
 
-    #alarmes{
-      width:1050px;
+    #graella_alarmes{
+      position: absolute;
+      width:950px;
       height:350px;
       cursor:pointer;
       transition: all 300ms ease;
     }
 
-    #hardware:hover{
-      width:675px;
-      height:450px;
-    }
-
-    #software{
-      width:425px;
-      height:425px;
+    #alarma_home{
+      position: absolute;
+      left:65px;
+      top:-55px;
+      width:175px;
+      height:200px;
       cursor:pointer;
       transition: all 300ms ease;
     }
 
-    #software:hover{
-      width:450px;
-      height:450px;
+    #alarma_home:hover{
+      width:190px;
+      height:215px;
     }
 
     </style>
   </head>
   <body>
     <div id="wrapp">
-      <section><img id="alarmes" src="graella_alarmes.png"/></section>
+      <section><img id="graella_alarmes" src="graella_alarmes.png"/></section>
+      <section><a href="home_alarm.php"><img id="alarma_home" src="alarma_home.png"/></a></section>
     </div>
   </body>
 </html>
