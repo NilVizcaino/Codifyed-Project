2f4b73d60e876fd6c38eb8fec4882dc3fcbdf755
kevincavi <kevincasanovasvila@gmail.com>
2022-04-08 17:57:13 +0200

    h

---
+++ Pagina web/productes_software.php
@@ -0,0 +1,38 @@
+<?php include("header.php") ?>
+
+<style>
+
+  div#wrapp{
+    display:grid;
+    position:relative;
+    width:70vw;
+    height:300px;
+    margin:-20px 110px 200px 125px;
+    grid-template-columns:1fr 1fr 1fr;
+    grid-column-gap:25px;
+  }
+
+
+  #codex:hover{
+    width:370px;
+    height:470px;
+  }
+
+
+  #codex_reforced:hover{
+    width:370px;
+    height:470px;
+  }
+
+  </style>
+</head>
+<body>
+  <div id="wrapp">
+    <section><a href="codex.php"><img id="codex" src="codex.png"/></a></section>
+    <section><a href="codex_premium.php"><img id="codex_premium" src="codex_premium.png"/></a></section>
+    <section><a href="codex_reforced.php"><img id="codex_reforced" src="codex_reforced.png"/></a></section>
+  </div>
+</body>
+</html>
+
+<?php include("footer.php") ?>
