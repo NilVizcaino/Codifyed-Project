67124a47013e7fa41d657455f94129f4ecec3c6a
Nil <kickcreator@gmail.com>
2022-04-28 20:38:18 +0200

    j

--- Pagina web/revisio_seguretat.php
+++ Pagina web/revisio_seguretat.php
@@ -27,21 +27,94 @@
       line-height:2.2rem;
     }
 
+    #envolcall_dades_personals{
+      height: 150px;
+      width: 400px;
+      position: absolute;
+      left:610px;
+      top:46vh;
+    }
+
+    input#Num_Telèfon{
+      font-size: 1.4rem;
+      height: 40px;
+      width: 210px;
+      font-family:"Josefin Sans", sans-serif ;
+      color:blue;
+      border-bottom-color: black;
+      border-top: none;
+      border-left: none;
+      border-right:  none;
+      outline:none;
+      margin-bottom: 20px;
+    }
+
+    input#Nom_i_Cognoms{
+      font-size: 1.4rem;
+      height: 40px;
+      width: 200px;
+      font-family:"Josefin Sans", sans-serif ;
+      color:blue;
+      border-bottom-color: black;
+      border-top: none;
+      border-left: none;
+      border-right:  none;
+      outline:none;
+      margin-bottom: 20px;
+    }
+
+    input#asumpte{
+      font-size: 1.4rem;
+      height: 40px;
+      width: 275px;
+      font-family:"Josefin Sans", sans-serif ;
+      color:blue;
+      border-bottom-color: black;
+      border-top: none;
+      border-left: none;
+      border-right:  none;
+      outline:none;
+      margin-bottom: 20px;
+    }
+
+    button#enviar{
+      position: absolute;
+      background-color:var(--verd);
+      top:80vh;
+      right:500px;
+      height: 32px;
+      width:130px;
+      font-size:1.25rem;
+      font-weight:700;
+      color:white;
+      line-height:2rem;
+      outline: none;
+    }
+
+    button#enviar:hover{
+      background-color: var(--turquessa);
+    }
+
     </style>
   </head>
   <body>
     <div id="titol">
-      <h1>Revisió de seguretat</h1>
+      <h1>Revisió de seguretat </h1>
     </div>
 
     <div id=div_paragref_qui_som>
     <p>En aquesta secció podrás contactar amb nosaltres per explicar-nos de una forma més detallada quin problema hi ha present en els teus dispositius. Recorda adjuntar captures i descripcións detallades del problema, a part de que és necesari que omplis totes les parts importants de informació per poder enviar la solicitud de revisió</p>
-
     <h2>Informació del problema:</h2>
-    
-    <textarea name="comentario" rows="14" cols="50"></textarea>
+    <textarea name="comentario" rows="14" cols="65"></textarea>
+
+    <div id="envolcall_dades_personals">
+    <h2>Dades generals:</h2>
+    <P>Asumpte:  <INPUT id="asumpte" type="text" name="asumpte">
+    <P>Nom i Cognoms:  <INPUT id="Nom_i_Cognoms" type="text" name="nom">
+    <P>Num de telèfon:  <INPUT id="Num_Telèfon" type="text" name="telefon">
+    </div>
+
+    <button type="button" id="enviar">enviar</button>
     </div>
   </body>
 </html>
-
-<?php include("footer.php") ?>
