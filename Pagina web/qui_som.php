b993d86f4709312bc3e9d992738b59c44b4ab9a9
kevincavi <kevincasanovasvila@gmail.com>
2022-05-03 20:46:10 +0200

    f

--- Pagina web/qui_som.php
+++ Pagina web/qui_som.php
@@ -1,10 +1,27 @@
 <?php include("header_user.php") ?>
 
+<style>
+
+#div_paragref_qui_som{
+  height: 400px;
+  width: auto;
+  margin: 30px 30px 30px 30px;
+  font-size: 2rem;
+  font-family: "Josefin Sans", sans-serif;
+}
+
+#eslogan{
+  font-size: 2rem;
+  font-family: "Josefin Sans", sans-serif;
+}
+
+</style>
+
 
 <div id=div_paragref_qui_som>
 <p>La nostra empresa es catalana, tenim botiga online i presencial situada al Carrer Muntaner, numero 507, Barcelona. La nostra empresa esta feta amb la intencio de ajudar i millorar la seguretat digital de tots els nostres clients.</p>
 <p>En cas de voler contectar amb nosaltres, podeu fer-ho a traves de les nostres xarxes socials, per Correu (CStaffSupport@service.com) o per Telefon (927 396 223).</p>
 </div>
 
-<div id="eslogan"><p>Nosaltres som codify-ed, la seguretat al teu costat.</p></div>
+<div id="eslogan"><p><b>Nosaltres som codify-ed, la seguretat al teu costat.</b></p></div>
 <?php include("footer.php") ?>
