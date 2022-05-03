469bc7a4f61643a30d8cef8df27b24e5fa954632
kevincavi <kevincasanovasvila@gmail.com>
2022-05-03 21:04:29 +0200

    N

--- Pagina web/codex_reforced.php
+++ Pagina web/codex_reforced.php
@@ -1,3 +1,138 @@
 <?php include("header_user.php") ?>
+<style>
 
+h1{
+  position: absolute;
+  top: 175px;
+  right: 260px;
+  font-size:3rem;
+}
+
+#foto{
+  position: absolute;
+  width: 430px;
+  height:500px;
+  top: 200px;
+  left: 105px;
+}
+
+#estrela1{
+  position: absolute;
+  top: 285px;
+  right: 665px;
+  width: 30px;
+  height: 30px;
+}
+
+#estrela2{
+  position: absolute;
+  top: 285px;
+  right: 630px;
+  width: 30px;
+  height: 30px;
+}
+
+#estrela3{
+  position: absolute;
+  top: 285px;
+  right: 595px;
+  width: 30px;
+  height: 30px;
+}
+
+#estrela4{
+  position: absolute;
+  top: 285px;
+  right: 560px;
+  width: 30px;
+  height: 30px;
+}
+
+#estrela5{
+  position: absolute;
+  top: 285px;
+  right: 525px;
+  width: 30px;
+  height: 30px;
+}
+
+#descripcio{
+  position: absolute;
+  top: 330px;
+  right: 45px;
+  width: 650px;
+  height: 30px;
+  font-size: 2rem;
+  font-family: "Josefin Sans", sans-serif;
+}
+
+#preu{
+  position: absolute;
+  top: 710px;
+  left:140px;
+}
+
+#pre{
+  color: #FAC209;
+  font-size: 3rem;
+  font-family: "Josefin Sans", sans-serif;
+}
+
+#carrito{
+  position: absolute;
+  background-color: var(--blau_cel);
+  color: white;
+  font-size: 1.5rem;
+  font-family: "Josefin Sans", sans-serif;
+  top: 770px;
+  left:140px;
+  width: 300px;
+  height: 50px;
+}
+
+#espai{
+  height: 750px;
+}
+
+</style>
+
+
+
+<body>
+
+  <h1>CODEX REFORCED</h1>
+
+  <img id="foto" src="codex_reforced.png"/>
+  <img id="estrela1" src="estrella.png"/>
+  <img id="estrela2" src="estrella.png"/>
+  <img id="estrela3" src="estrella.png"/>
+  <img id="estrela4" src="estrella.png"/>
+  <div id="descripcio">
+
+
+
+<p>- Actualització automática de base de dades.</p>
+<p>- Escanejat de virus en temps real.</p>
+<p>- Bloqueig automatic de llocs sospitosos.</p>
+<p>- Eines d’asistencia remota.</p>
+<p>- Métodos de recuperació de dades.</p>
+<p>- Antiphishing i antifraud.</p>
+<p>- Té una base dades mes actual i precissa.</p>
+<p>- Fa revisions mes precisses.</p>
+<p>- Té un sistema de scanner automatic fitxer que ingressa a l’equip.</p>
+<p>- Si troba algun virus actualitzara automaticament la seva base de dades local.</p>
+</div>
+
+<div id="preu">
+
+<h8 id="pre"><b>35€ al mes</b></h8>
+
+</div>
+
+<button id="carrito"><b>Afegir a la cistella</b></button>
+
+<div id="espai"></div>
+
+
+</body>
 <?php include("footer.php") ?>
