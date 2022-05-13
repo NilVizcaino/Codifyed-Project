3d8c5ed35ea55bbd78b874e7c2aad484ff4b38ab
Nil <kickcreator@gmail.com>
2022-05-13 17:50:04 +0200

    ciy

--- Pagina web/comprar.php
+++ Pagina web/comprar.php
@@ -9,7 +9,7 @@
       display:grid;
       position:relative;
       width:50vw;
-      height:700px;
+      height:600px;
       margin:0px auto 200px auto;
       background-color: var(--blau_cel);
       grid-template-columns: 30% calc(70% - 10px);
@@ -33,7 +33,7 @@
       display:flex;
       position:relative;
       height: auto;
-      background-color:;
+      background-color:transparent;
       margin:10px 10px 10px 10px;
       border-radius: 10px;
     }
@@ -96,6 +96,30 @@
       font-family: "Josefin Sans", sans-serif;
     }
 
+    #Titol_Producte{
+      display:block;
+      position:absolute;
+      width:400px;
+      height:60px;
+      color:var(--verd);
+      left:0vw;
+      top:2vh;
+      font-size: 2rem;
+      font-family: "Josefin Sans", sans-serif;
+    }
+
+    #Preu_Producte{
+      display:block;
+      position:absolute;
+      width:400px;
+      height:60px;
+      left:0vw;
+      top:8vh;
+      color: white;
+      font-size: 1.5rem;
+      font-family: "Josefin Sans", sans-serif;
+    }
+
 
     </style>
   </head>
@@ -103,10 +127,16 @@
 
     <div id="wrapp">
       <div id="foto-content-antivirus"><img id="foto" src="codex.png"/></div>
-      <div id="contenido">CODEX Antivirus</div>
+      <div id="contenido">
+        <div id="Titol_Producte"><b>CODEX</b></div>
+        <div id="Preu_Producte"><b>15€ al mes</b></div>
+      </div>
 
       <div id="foto-content"><img id="foto_sai" src="coneptronic_sai_nobackground.png"/></div>
-      <div id="contenido">CONCEPTRONIC</div>
+      <div id="contenido">
+        <div id="Titol_Producte"><b>CONCEPTRONIC</b></div>
+        <div id="Preu_Producte"><b>200€</b></div>
+      </div>
 
       <h11><b>Seleccioni un metode de pagament:</b></h11>
 
